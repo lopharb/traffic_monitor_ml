075540f1b68f70ef20b48303e7cb21e49d2f7a83
Ivan Shkel <skillofivan@gmail.com>
2025-05-17 20:25:26 +0300

    [api] add middleware to handle FE requests

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from app.api.endpoints import detector_router
 from fastapi import FastAPI
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(
     title="Transport Monitoring Tool API",
     description="API for Transport Monitoring Tool",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(detector_router)
